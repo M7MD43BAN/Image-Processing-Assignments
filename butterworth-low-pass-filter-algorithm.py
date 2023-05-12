87e0f757c66a353fd254be2e4f46f695bde31f85
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-05-12 09:31:42 +0300

    implement butterworth-low-pass-filter-algorithm.py

---
+++ butterworth-low-pass-filter-algorithm.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+
+def butterworth_low_pass(image, radius, n):
+    [row, column, channel] = image.shape
+
+    dft = np.fft.fft2(image, axes=(0, 1))
+    dft_shift = np.fft.fftshift(dft)
+
+    mask = np.zeros_like(image)
+
+    for k in range(0, channel):
+        for i in range(0, row):
+            for j in range(0, column):
+                distance = int(((((i - (row / 2)) ** 2) + ((j - (column / 2)) ** 2)) ** 0.5))
+                temp = (1 / (1 + (distance / radius) ** (2 * n))) * 255
+                mask[i, j, k] = temp
+
+    dft_shift_masked = np.multiply(dft_shift, mask) / 255
+
+    back_is_shift_masked = np.fft.ifftshift(dft_shift_masked)
+    img_filtered = np.fft.ifft2(back_is_shift_masked, axes=(0, 1))
+    img_filtered = np.abs(img_filtered).clip(0, 255).astype(np.uint8)
+
+    return img_filtered, mask
+
+original_image = cv2.imread("Squidward.jpeg")
+new_img, mask = butterworth_low_pass(original_image, 20, 2)
+
+cv2.imshow('Original Image', original_image)
+cv2.imshow('Mask', mask)
+cv2.imshow('Filtered Image', new_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
